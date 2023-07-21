43bfa4478de9381ddf2d47cf8a60a5656656df2c
Mehuldesai99 <mehul.desai7600@gmail.com>
2023-07-21 22:19:25 +0530

    Module_4
    OOPS Concept

---
+++ oop.cpp
@@ -0,0 +1,15 @@
+What is OOP?List OOP concepts
+answer:-
+
+The programming paradigm known as "Object-Oriented Programming" works with objects and classes. In a subsequent
+section of this article, we will talk about objects and classes. For the time being, we can state that object-
+oriented programming is a style of programming that, by emphasizing real-world elements (called objects), 
+greatly simplifies programming.
+
+
+>> Object-Oriented Programming has 4 main Pillars <<
+
+-> Inheritance
+-> Polymorphism
+-> Abstraction
+-> Encapsulation
---
+++ oop_and_pop.cpp
@@ -0,0 +1,27 @@
+         OOP	                                                              POP
+
+>> Object oriented.	                                             >> Structure oriented.
+
+>> Program is divided into objects.	                              >> Program is divided into functions.
+
+>> Bottom-up approach.	                                          >> Top-down approach.
+
+>> Inheritance property is used.	                                 >> Inheritance is not allowed.
+
+>> It uses access specifier.	                                    >> It doesn’t use access specifier.
+
+>> Encapsulation is used to hide the data.             	         >> No data hiding.
+
+>> Concept of virtual function.	                                 >> No virtual function.
+
+>> Object functions are linked                                    >> Parts of program are linked through
+   through message passing.                                          parameter passing.
+
+>> Adding new data and functions             	                  >> Expanding new data and functions is 
+   is easy.                                                          not easy.
+
+>> The existing code can be reused.	                              >> No code reusability.
+
+>> use for solving big problems.	                                 >> Not suitable for solving big problems.
+
+>> C++, Java.                                              	      >> C, Pascal.
